488322580a431fb7e668ab8634a07f92d08eb354
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2023-02-09 20:14:43 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++)
+        if (!str.includes(bracketsConfig[i].join(''))) {
+          return i;
+        } else {
+          str = str.replace(bracketsConfig[i].join(''), '');
+        }
+
+        
+    if (str.length === 0) {
+      return true;
+    } else {
+      return false;
+  }
 }
